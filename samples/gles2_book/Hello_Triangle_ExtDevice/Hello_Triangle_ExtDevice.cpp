fa0fc6b0d232c38a5e86530043196f4db5578c8d
stoyan <stoyan@coherent-labs.com>
2013-06-24 12:47:22 +0300

    * Sample for client-supplied rendering device

---
+++ samples/gles2_book/Hello_Triangle_ExtDevice/Hello_Triangle_ExtDevice.cpp
@@ -0,0 +1,394 @@
+#include <stdlib.h>
+#define WIN32_LEAN_AND_MEAN
+#include <d3d9.h>
+
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
+#define WIDTH 640
+#define HEIGHT 480
+
+struct _Context {
+	~_Context() {
+		if(Device) Device->Release();
+		if(D3D) D3D->Release();
+	}
+	HWND hWnd;
+	HMODULE D3DModule;
+	IDirect3D9* D3D;
+	IDirect3DDevice9* Device;
+
+	EGLDisplay Display;
+	EGLSurface Surface;
+	EGLContext Context;
+	GLuint ProgramObject;
+
+	PFNEGLGETDISPLAYANGLE eglGetDisplayANGLE;
+} g_Context = {0};
+
+///
+// Create a shader object, load the shader source, and
+// compile the shader.
+//
+GLuint LoadShader ( GLenum type, const char *shaderSrc )
+{
+   GLuint shader;
+   GLint compiled;
+   
+   // Create the shader object
+   shader = glCreateShader ( type );
+
+   if ( shader == 0 )
+   	return 0;
+
+   // Load the shader source
+   glShaderSource ( shader, 1, &shaderSrc, NULL );
+   
+   // Compile the shader
+   glCompileShader ( shader );
+
+   // Check the compile status
+   glGetShaderiv ( shader, GL_COMPILE_STATUS, &compiled );
+
+   if ( !compiled ) 
+   {
+      GLint infoLen = 0;
+
+      glGetShaderiv ( shader, GL_INFO_LOG_LENGTH, &infoLen );
+      
+      if ( infoLen > 1 )
+      {
+         char* infoLog = static_cast<char*>(malloc(sizeof(char) * infoLen ));
+
+         glGetShaderInfoLog ( shader, infoLen, NULL, infoLog );
+         
+         free ( infoLog );
+      }
+
+      glDeleteShader ( shader );
+      return 0;
+   }
+
+   return shader;
+}
+
+///
+// Initialize the shader and program object
+//
+int Init ()
+{
+   const char vShaderStr[] =  
+      "attribute vec4 vPosition;    \n"
+      "void main()                  \n"
+      "{                            \n"
+      "   gl_Position = vPosition;  \n"
+      "}                            \n";
+   
+   const char fShaderStr[] =  
+      "precision mediump float;\n"\
+      "void main()                                  \n"
+      "{                                            \n"
+      "  gl_FragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );\n"
+      "}                                            \n";
+
+   GLuint vertexShader;
+   GLuint fragmentShader;
+   GLuint programObject;
+   GLint linked;
+
+   // Load the vertex/fragment shaders
+   vertexShader = LoadShader ( GL_VERTEX_SHADER, vShaderStr );
+   fragmentShader = LoadShader ( GL_FRAGMENT_SHADER, fShaderStr );
+
+   // Create the program object
+   programObject = glCreateProgram ( );
+   
+   if ( programObject == 0 )
+      return 0;
+
+   glAttachShader ( programObject, vertexShader );
+   glAttachShader ( programObject, fragmentShader );
+
+   // Bind vPosition to attribute 0   
+   glBindAttribLocation ( programObject, 0, "vPosition" );
+
+   // Link the program
+   glLinkProgram ( programObject );
+
+   // Check the link status
+   glGetProgramiv ( programObject, GL_LINK_STATUS, &linked );
+
+   if ( !linked ) 
+   {
+      GLint infoLen = 0;
+
+      glGetProgramiv ( programObject, GL_INFO_LOG_LENGTH, &infoLen );
+      
+      if ( infoLen > 1 )
+      {
+         char* infoLog = static_cast<char*>(malloc (sizeof(char) * infoLen ));
+
+         glGetProgramInfoLog ( programObject, infoLen, NULL, infoLog );
+         
+         free ( infoLog );
+      }
+
+      glDeleteProgram ( programObject );
+      return FALSE;
+   }
+
+   // Store the program object
+   g_Context.ProgramObject = programObject;
+
+   glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
+   return TRUE;
+}
+
+///
+// Draw a triangle using the shader pair created in Init()
+//
+void Draw ( )
+{
+   GLfloat vVertices[] = {  0.0f,  0.5f, 0.0f, 
+                           -0.5f, -0.5f, 0.0f,
+                            0.5f, -0.5f, 0.0f };
+      
+   // Set the viewport
+   glViewport ( 0, 0, WIDTH, HEIGHT );
+   
+   // Clear the color buffer
+   glClear ( GL_COLOR_BUFFER_BIT );
+
+   // Use the program object
+   glUseProgram ( g_Context.ProgramObject );
+
+   // Load the vertex data
+   glVertexAttribPointer ( 0, 3, GL_FLOAT, GL_FALSE, 0, vVertices );
+   glEnableVertexAttribArray ( 0 );
+
+   glDrawArrays ( GL_TRIANGLES, 0, 3 );
+
+   eglSwapBuffers ( g_Context.Display, g_Context.Surface );
+}
+
+LRESULT WINAPI WindowProc ( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam ) 
+{
+   LRESULT  lRet = 1; 
+
+   switch (uMsg) 
+   { 
+      case WM_CREATE:
+         break;
+      case WM_PAINT:
+         {
+            Draw();
+            ValidateRect(hWnd, NULL);
+         }
+         break;
+      case WM_DESTROY:
+         PostQuitMessage(0);             
+         break; 
+      default: 
+         lRet = DefWindowProc (hWnd, uMsg, wParam, lParam); 
+         break; 
+   } 
+
+   return lRet; 
+}
+
+HWND CreateWin(unsigned width, unsigned height, const char* title)
+{
+	WNDCLASS wndclass = {0}; 
+	DWORD    wStyle   = 0;
+	RECT     windowRect;
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+	
+	wndclass.style         = CS_OWNDC;
+	wndclass.lpfnWndProc   = (WNDPROC)WindowProc; 
+	wndclass.hInstance     = hInstance; 
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH); 
+	wndclass.lpszClassName = TEXT("opengles2.0");
+	
+	if (!RegisterClass (&wndclass) ) 
+	   return FALSE; 
+	
+	wStyle = WS_VISIBLE | WS_POPUP | WS_BORDER | WS_SYSMENU | WS_CAPTION | WS_SIZEBOX;
+	
+	// Adjust the window rectangle so that the client area has
+	// the correct number of pixels
+	windowRect.left = 0;
+	windowRect.top = 0;
+	windowRect.right = width;
+	windowRect.bottom = height;
+	
+	AdjustWindowRect ( &windowRect, wStyle, FALSE );
+	
+	HWND hWnd = CreateWindow(
+	                      TEXT("opengles2.0"),
+	                      title,
+	                      wStyle,
+	                      0,
+	                      0,
+	                      windowRect.right - windowRect.left,
+	                      windowRect.bottom - windowRect.top,
+	                      NULL,
+	                      NULL,
+	                      hInstance,
+	                      NULL);
+	
+	if(!hWnd)
+		return NULL;
+
+	ShowWindow(hWnd, TRUE);
+	
+	return hWnd;
+}
+
+void WinLoop ()
+{
+   MSG msg = { 0 };
+   int done = 0;
+   DWORD lastTime = GetTickCount();
+   
+   while (!done)
+   {
+      int gotMsg = (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) != 0);
+      if ( gotMsg )
+      {
+         if (msg.message==WM_QUIT)
+         {
+             done=1; 
+         }
+         else
+         {
+             TranslateMessage(&msg); 
+             DispatchMessage(&msg); 
+         }
+      }
+      else {
+         SendMessage( g_Context.hWnd, WM_PAINT, 0, 0 );
+	  }
+   }
+}
+
+bool InitializeDirectX(){
+	g_Context.D3DModule = LoadLibrary("d3d9.dll");
+	if(!g_Context.D3DModule) 
+		return false;
+
+	g_Context.D3D = ::Direct3DCreate9(D3D_SDK_VERSION);
+	if(!g_Context.D3D)
+		return false;
+
+	// Initialize with a dummy window
+	HWND window = GetShellWindow();
+	D3DPRESENT_PARAMETERS presentParams = {0};
+	presentParams.BackBufferWidth = 1;
+	presentParams.BackBufferHeight = 1;
+	presentParams.BackBufferCount = 1;
+	presentParams.BackBufferFormat = D3DFMT_A8R8G8B8;
+	presentParams.hDeviceWindow = window;
+	presentParams.Windowed = TRUE;
+	presentParams.Flags = 0;
+	presentParams.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;
+	presentParams.SwapEffect = D3DSWAPEFFECT_DISCARD;
+
+	HRESULT hr = g_Context.D3D->CreateDevice(
+					D3DADAPTER_DEFAULT
+					, D3DDEVTYPE_HAL
+					, window
+					, D3DCREATE_FPU_PRESERVE | D3DCREATE_HARDWARE_VERTEXPROCESSING
+					, &presentParams
+					, &g_Context.Device);
+
+	if(FAILED(hr))
+		return false;
+	
+	return true;
+}
+
+bool InitializeGLES() {
+	g_Context.eglGetDisplayANGLE = (PFNEGLGETDISPLAYANGLE) eglGetProcAddress("eglGetDisplayANGLE");
+	g_Context.Display = g_Context.eglGetDisplayANGLE(EGL_ANGLE_D3D9_DISPLAY_DEVICE, g_Context.Device);
+	
+	if(!eglInitialize(g_Context.Display, NULL, NULL))
+	   return false;
+
+	static const EGLint configAttribs[] = {
+		EGL_BUFFER_SIZE, 32,
+		EGL_ALPHA_SIZE, 8,
+		EGL_BLUE_SIZE, 8,
+		EGL_GREEN_SIZE, 8,
+		EGL_RED_SIZE, 8,
+		EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+		EGL_SURFACE_TYPE, EGL_WINDOW_BIT | EGL_PBUFFER_BIT,
+		EGL_NONE
+	};
+
+	static EGLint surfaceAttribList[] =
+	{
+		EGL_POST_SUB_BUFFER_SUPPORTED_NV, EGL_TRUE,
+		EGL_NONE, EGL_NONE
+	};
+
+	EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE, EGL_NONE };
+
+	EGLint configsCnt = 0;
+	if (!eglChooseConfig(g_Context.Display,
+	                     configAttribs,
+	                     NULL,
+	                     0,
+	                     &configsCnt)) {
+		return false;
+	}
+	
+	if (configsCnt == 0) {
+		return false;
+	}
+	
+	EGLConfig config;
+	EGLint numConfigs;
+	if (!eglChooseConfig(g_Context.Display,
+	                     configAttribs,
+	                     &config,
+	                     1,
+	                     &numConfigs)) {
+		return false;
+	}
+
+	g_Context.Surface = eglCreateWindowSurface(g_Context.Display, config, (EGLNativeWindowType)g_Context.hWnd, surfaceAttribList);
+	if(g_Context.Surface == EGL_NO_SURFACE)
+	   return false;
+	// Create a GL context
+	g_Context.Context = eglCreateContext(g_Context.Display, config, EGL_NO_CONTEXT, contextAttribs);
+	if(g_Context.Context == EGL_NO_CONTEXT)
+		return false;
+	
+	// Make the context current
+	if (!eglMakeCurrent(g_Context.Display, g_Context.Surface, g_Context.Surface, g_Context.Context))
+		return false;
+	
+	return true;
+}
+
+int main ( int argc, char *argv[] )
+{
+	g_Context.hWnd = CreateWin(640, 480, "Hello_Triangle_ExtDevice");
+	if(!g_Context.hWnd)
+		return 1;
+
+	if(!InitializeDirectX())
+		return 2;
+
+	if(!InitializeGLES())
+		return 3;
+
+	if(!Init())
+		return false;
+
+	WinLoop();
+
+	return 0;   
+}
